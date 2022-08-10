dc82acc22b2cd4a191e32881794b3a538fbbca32
Dmitry M <a1a1a123@mail.ru>
2022-08-10 15:36:17 +0500

    change 'try' to 'with'

--- гугл.py
+++ гугл.py
@@ -2,19 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-try:
-    browser = webdriver.Chrome()
+with webdriver.Chrome() as browser:
     browser.get("https://www.google.ru/")
-    element = browser.find_element(By.TAG_NAME, "input")
-    element.send_keys("Как разобрать пылесос?")
-    time.sleep(2)
-    button = browser.find_element(By.CLASS_NAME, "gNO89b")
-    button.click()
-
-finally:
-    # успеваем скопировать код за 30 секунд
+    browser.implicitly_wait(10)
+    browser.find_element(By.TAG_NAME, "input").send_keys("Как гуглить?")
+    browser.find_element(By.CLASS_NAME, "gNO89b").click()
     time.sleep(10)
-    # закрываем браузер после всех манипуляций
     browser.quit()
-
-# не забываем оставить пустую строку в конце файла
