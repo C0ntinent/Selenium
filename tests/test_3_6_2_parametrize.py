b2796004735dc6e5c7367375e1122e7eb4c761d4
Dmitry M <a1a1a123@mail.ru>
2022-08-11 16:05:53 +0500

    rename files

--- tests/test_3_6_2_parametrize.py
+++ tests/test_3_6_2_parametrize.py
@@ -16,16 +16,20 @@ def browser():
     print(phrase)
 
 
+@pytest.fixture(scope="function")
+def answer():
+    return str(math.log(int(time.time())))
+
+
 phrase = ''
 
 
 @pytest.mark.parametrize('number', ["895", "896", "897", "898", "899", "903", "904", "905"])
-def test_guest_should_see_login_link(browser, number, final=None):
+def test_guest_should_see_login_link(browser, number, answer):
     global phrase
     link = f"https://stepik.org/lesson/236{number}/step/1/"
     browser.implicitly_wait(10)
     browser.get(link)
-    answer = math.log(int(time.time()))
     browser.find_element(By.TAG_NAME, "textarea").send_keys(answer)
     browser.find_element(By.CLASS_NAME, "submit-submission").click()
     word = browser.find_element(By.CLASS_NAME, "smart-hints__hint").text
